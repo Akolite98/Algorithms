50029df246b9ea03bbea14858b9b08c477810002
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2022-10-09 00:34:13 +0100

    twerked arrays a bit

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var array1 = ["Eat", "the", "Food"];
 var array2 = ["The", "Food", "is", "not", "delicious"];
 
-console.log(array1.concat(array2));
+// console.log(array1.concat(array2));
+console.log(array1.splice(1));
